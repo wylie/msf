380d5f95941e2216ef5aecaaacbf121521c91672
Wylie Fisher <me@wyliefisher.com>
2025-06-03 20:25:39 -0400

    trying to fix the images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://marksfisher.com', // Change to your actual domain
-  base: '/msf/' // You seem to be using this base path in your image URLs
+  site: 'https://marksfisher.com'
 });
